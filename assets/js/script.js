bd6373f626d729addc03706ad0c25bf3e796fd16
robertdavid1205 <robertdavid1205@gmail.com>
2021-08-02 12:48:07 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
